f0753604fcfcf2f4238e76dde41efe42c3b7b8c6
Shonkhogrib <161463831+Shonkhogrib@users.noreply.github.com>
2025-07-29 03:52:27 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function getRandomNumber(a, b) {
+    return Math.floor(Math.random() * (b - a + 1)) + a;
+}
+
+function getRandom(anyArray) {
+    let max = anyArray.length - 1;
+    let min = 0;
+    let random = getRandomNumber(min, max);
+    return anyArray[random];
+}
+
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed on', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // write your code here
+  let excuse =    
+    getRandom(who) + " " +
+    getRandom(action) + " " +
+    getRandom(what) + " " +
+    getRandom(when) + ".";
+  document.querySelector("#excuse").innerHTML = excuse;
+  document.querySelector('#generate-btn').addEventListener('click', function() {
+    let newExcuse =    
+      getRandom(who) + " " +
+      getRandom(action) + " " +
+      getRandom(what) + " " +
+      getRandom(when) + ".";
+    document.querySelector("#excuse").innerHTML = newExcuse; 
+  }); 
 };
